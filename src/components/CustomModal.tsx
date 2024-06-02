829a746b499a38ac5cebb56ee3c492648cd4c535
AmirHabibi79 <www.habibiamir883@gmail.com>
2024-06-02 21:04:47 +0330

    Change the modal to use Dialog instead

--- src/components/CustomModal.tsx
+++ src/components/CustomModal.tsx
@@ -1,14 +1,19 @@
-import { Modal } from "@mui/material";
+import { Dialog } from "@mui/material";
+import type { DialogProps } from "@mui/material";
 import React from "react";
 type props = React.PropsWithChildren<{ open: boolean; onClose: () => void }>;
 
 export default function CustomModal({ children, open, onClose }: props) {
+  const handleClose: DialogProps["onClose"] = (_, reason) => {
+    if (reason && reason === "backdropClick") {
+      onClose();
+    }
+  };
   return (
-    <Modal open={open} onClose={onClose}>
+    <Dialog open={open} onClose={handleClose}>
       <div className="w-full h-full flex items-center justify-center">
-        {" "}
-        {children as any}
+        {children}
       </div>
-    </Modal>
+    </Dialog>
   );
 }
