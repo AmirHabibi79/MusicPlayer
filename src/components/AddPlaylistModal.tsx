82996c2fcd5262e70129288c8bd559d462d71160
AmirHabibi79 <www.habibiamir883@gmail.com>
2024-06-02 21:06:23 +0330

    some changes

--- src/components/AddPlaylistModal.tsx
+++ src/components/AddPlaylistModal.tsx
@@ -1,17 +1,21 @@
-import { LegacyRef, useRef, useState } from "react";
+import { LegacyRef, useEffect, useRef, useState } from "react";
 import CustomModal from "./CustomModal";
 import usePlaylist from "../hooks/usePlaylist";
 export default function AddPlaylistModal() {
   const [open, setOpen] = useState(false);
   const { addPlayList } = usePlaylist();
   const nameRef = useRef<HTMLInputElement>();
+  useEffect(() => {
+    nameRef.current?.focus();
+  }, []);
   return (
     <>
-      <CustomModal open={open} onClose={() => {}}>
+      <CustomModal open={open} onClose={() => setOpen(false)}>
         <div className="flex flex-col gap-2 p-2 bg-white">
           <input
             ref={nameRef as LegacyRef<HTMLInputElement>}
             placeholder="name of playlist"
+            autoFocus={true}
           />
           <button
             onClick={() => {
